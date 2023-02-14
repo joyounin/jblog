f45a846095f837a95cca4cde4c77e5fa05256ee9
joyounin <3723519@naver.com>
2023-02-14 14:13:53 +0900

    jblog3 #finish

--- jblog03/src/main/java/com/douzone/jblog/controller/UserController.java
+++ jblog03/src/main/java/com/douzone/jblog/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.douzone.jblog.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -27,11 +32,16 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.GET)
-	public String join() {
+	public String join(@ModelAttribute UserVo vo) {
 		return "user/join";
 	}
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(UserVo vo) {
+	public String join(@ModelAttribute @Valid UserVo vo, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAllAttributes(result.getModel());
+			return "user/join";
+		}
+		
 		userService.join(vo);
 		blogService.insert(vo);
 		categoryService.insert(vo);
