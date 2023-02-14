ad36cc483efc3eee841c4ca00d7bff26b62998c0
joyounin <3723519@naver.com>
2023-02-14 17:02:12 +0900

    jblog05 #finish

--- jblog05/src/main/java/com/douzone/jblog/exception/GlobalExceptionHandler.java
+++ jblog05/src/main/java/com/douzone/jblog/exception/GlobalExceptionHandler.java
@@ -8,22 +8,30 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	
+
 	private static final Log logger = LogFactory.getLog(GlobalExceptionHandler.class);
-	
+
 	@ExceptionHandler(Exception.class)
 	public ModelAndView handlerException(Exception e) {
-		//1. 로깅(Logging)
+		// 1. 404 Error 처리
+		if (e instanceof NoHandlerFoundException) {
+			ModelAndView mav = new ModelAndView();
+			mav.setViewName("error/404");
+
+			return mav;
+		}
+		// 2. 로깅(Logging)
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
-		
+
 		logger.error(errors.toString());
-		
-		//2. 사과 페이지(3. 정상종료)
-		
+
+		// 3. 사과 페이지(3. 정상종료)
+
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("exception", "죄송합니다 다시 접속해주세요.");
 		mav.setViewName("error/exception");
